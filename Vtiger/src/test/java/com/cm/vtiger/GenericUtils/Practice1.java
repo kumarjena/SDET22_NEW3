1e188f38aa2f5c2df2904be7e9f4e8627408ed51
Mahendra Kumar <Mahendra Kumar@LAPTOP-2G2K9K3L>
2021-10-19 15:16:35 +0530

    FINAL 1

---
+++ Vtiger/src/test/java/com/cm/vtiger/GenericUtils/Practice1.java
@@ -0,0 +1,12 @@
+package com.cm.vtiger.GenericUtils;
+
+public class Practice1 {
+	public static void main(String[] args) {
+		System.out.println("Mahendra Kumar");
+		
+		System.out.println("ELOCTRONICCITY");
+		
+		System.out.println("CREATE SUCCESSFULLY");
+	}
+
+}
